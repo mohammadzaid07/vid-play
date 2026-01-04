8ee1890201a7fcd8fa08d18d15fbe6cf570a1c9b
Mohammad Zaid <mohdzaid07091997@gmail.com>
2026-01-04 17:24:53 +0530

    Mongo DB Connected

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./db/index.js";
+import express from "express";
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+(async () => {
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`
+        );
+
+        console.log(
+            `✅ MongoDB connected | DB: ${connectionInstance.connection.host}`
+        );
+
+        app.on("error", (error) => {
+            console.error("❌ Express error:", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`🚀 App is listening on port ${process.env.PORT}`);
+        });
+
+    } catch (error) {
+        console.error("❌ Startup error:", error.message);
+        process.exit(1);
+    }
+})();
+*/
